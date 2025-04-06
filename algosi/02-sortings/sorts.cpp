b154d0f01b01fbd8acaea25e93032858450ee834
danilasar <danila.sar@yandex.ru>
2025-04-06 23:38:17 +0400

    фича: сортировка слиянием

--- algosi/02-sortings/sorts.cpp
+++ algosi/02-sortings/sorts.cpp
@@ -91,6 +91,46 @@ void quick_sort(std::vector<int>& arr) {
 	sort(arr);
 }
 
+void merge(std::vector<int>::iterator array, size_t left, size_t mid_index, size_t right) {
+	auto mid = array + mid_index;
+	auto end = array + right;
+	size_t full_size = right - left;
+	auto it1 = array;
+	auto it2 = array;
+	std::vector<int> result(full_size);
+	while(it1 < mid && it2 < end) {
+		if(*it1 < *it2) {
+			result[(it1 - array) + (it2 - array)] = *it1;
+			++it1;
+		} else {
+			result[(it1 - array) + (it2 - array)] = *mid;
+			++it2;
+		}
+	}
+	while(it1 < mid) {
+		result[(it1 - array) + (it2 - array)] = *it1;
+		++it1;
+	}
+	while(it2 < end) {
+		result[(it1 - array) + (it2 - array)] = *it2;
+		++it2;
+	}
+	std::copy(result.begin(), result.end(), array);
+}
+
+void merge_sort(std::vector<int>::iterator begin, std::vector<int>::iterator end) {
+	size_t size = end - begin;
+	for(size_t i = 1; i < size; i *= 2) {
+		for(size_t j = 0; j < size - i; j += 2 * i) {
+			merge(begin, j, j + i, std::min(j + 2 * i, size));
+		}
+	}
+}
+
+void heap_sort(std::vector<int>& arr) {
+
+}
+
 int main(int argc, char* argv[]) {
 	if (argc != 2) {
 		std::cerr << "Usage: " << argv[0] << " [counting|radix|quick|merge|heap]\n";
@@ -106,8 +146,8 @@ int main(int argc, char* argv[]) {
 	if (mode == "counting") counting_sort(v);
 	else if (mode == "radix") radix_sort_lsd(v);
 	else if(mode == "quck") quick_sort(v);
-	else if(mode == "merge") merge_sort(v);
-	else if(mode == " heap") heap_sort(v);
+	else if(mode == "merge") merge_sort(v.begin(), v.end());
+	else if(mode == "heap") heap_sort(v);
 	else {
 		std::cerr << "Invalid mode. Use 'counting' or 'radix'.\n";
 		return 1;
