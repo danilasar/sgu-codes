d2eabd3e61870f1ef87b958c0f16cb559432bf7d
danilasar <danila.sar@yandex.ru>
2025-04-24 02:34:03 +0400

    фича(алгосы): быстрая сортировка

--- algosi/02-sortings/src/sortings/quick.cpp
+++ algosi/02-sortings/src/sortings/quick.cpp
@@ -1,20 +1,34 @@
 #include "sortings.h"
+#include <functional>
 #include <random>
+#include <vector>
+
 namespace Sortings {
-	void quick_sort(std::vector<int>& arr) {
+	void quick_sort(std::vector<int>& arr, bool debug) {
 		std::random_device rdev;
 		std::default_random_engine rengine(rdev());
-		auto sort = [&rengine](std::vector<int> &arr) -> std::vector<int>& {
-			if(arr.size() <= 1) {
-				return arr;
+		if(arr.size() <= 1) {
+			return;
+		}
+		std::function<void(int, int)> partition_sort;
+		partition_sort = [&arr, &rengine, &partition_sort](int low, int high) {
+			if(low >= high) return;
+			std::uniform_int_distribution<> unidist(0, high - low);
+			int pivot_index = low + unidist(rengine);
+			int pivot = arr[pivot_index];
+			std::swap(arr[pivot_index], arr[high]);
+			int i = low - 1;
+			for(int j = low; j < high; ++j) {
+				if(arr[j] <= pivot) {
+					i++;
+					std::swap(arr[i], arr[j]);
+				}
 			}
-			std::uniform_int_distribution<> unidist(0, arr.size());
-			int mean = unidist(rengine);
-			// TODO:
-			
-			std::vector<int> *left = new std::vector<int>(arr.begin(), arr.end());
-			return *(new std::vector<int>());
+			std::swap(arr[i + 1], arr[high]);
+			int pivot_pos = i + 1;
+			partition_sort(low, pivot_pos - 1);
+			partition_sort(pivot_pos + 1, high);
 		};
-		sort(arr);
+		partition_sort(0, arr.size() - 1);
 	}
 }
