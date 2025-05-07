fb833f7295edac9feaca792d4778b9a3899bbff7
danilasar <danila.sar@yandex.ru>
2025-05-07 15:46:32 +0400

    рутина(кг): возрождение 2d

---
+++ kg/raylib/Projects/lab5_2d/main.cpp
@@ -0,0 +1,221 @@
+#include "matrix.hpp"
+#include "transform.hpp"
+#include "figure.hpp"
+
+#include "hare.hpp"
+
+#include <raylib.h>
+#include <raygui.h>
+#include <nfd.h>
+#include <vector>
+#include <array>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdint>
+
+char codeKS(const Vec2& P, const Vec2& min, const Vec2& max) {
+	char code = 0;
+
+	if(P.x < min.x) {
+		code |= 1;
+	} else if(P.x > max.x) {
+		code |= 2;
+	}
+
+	if(P.y < min.y) {
+		code |= 4;
+	} else if(P.y > max.y) {
+		code |= 8;
+	}
+
+	return code;
+}
+
+bool clip(Vec2 &A, Vec2 &B, const Vec2& min, const Vec2& max) {
+	char codeA = codeKS(A, min, max);
+	char codeB = codeKS(B, min, max);
+
+	while(codeA | codeB) {
+		if(codeA & codeB) {
+			return false;
+		}
+		if(codeA == 0) {
+			std::swap(A, B);
+			std::swap(codeA, codeB);
+		}
+
+		if(codeA & 1) { // A левее области видимости
+			A.y = A.y + (B.y - A.y) * (min.x - A.x) / (B.x - A.x);
+			A.x = min.x;
+		} else if(codeA & 2) { // A правее области видимости
+			A.y = A.y + (B.y - A.y) * (max.x- A.x) / (B.x - A.x);
+			A.x = max.x;
+		} else if(codeA & 4) { // A ниже области видимости
+			A.x = A.x + (B.x - A.x) * (min.y - A.y) / (B.y - A.y);
+			A.y = min.y;
+		} else { // A выше области видимости
+			A.x = A.x + (B.x - A.x) * (max.y - A.y) / (B.y - A.y);
+			A.y = max.y;
+		}
+		codeA = codeKS(A, min, max);
+	}
+	return true;
+}
+
+struct Padding {
+	float left, right, top, bottom;
+};
+
+void frame_calc(const Padding& p, float& Wx, float& Wy, float& Wcx, float& Wcy, float& frameAspect) {
+	Wx = static_cast<float>(GetScreenWidth()) - p.left - p.right;
+	Wy = static_cast<float>(GetScreenHeight()) - p.top - p.bottom;
+	Wcx = p.left;
+	Wcy = p.top + Wy;
+	frameAspect = Wx / Wy;
+}
+
+float f(float x) {
+	//return x * sin(log(x));
+	return tan(x);
+}
+
+bool f_exists(float x, float delta_x) {
+	//return x > 0;
+	//return cos(x) != 0.f;
+	return fabs(2.f * acos(cos(x)) - M_PI) > delta_x;
+}
+
+int main() {
+	if (NFD_Init() != NFD_OKAY) {
+		std::cerr << "ERROR: can't initialize Native File Dialog" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(600, 600, "Lab cuatro");
+	SetTargetFPS(60);
+
+	Mat3 T = Mat3(1.f);
+	Mat3 initT;
+
+	const Padding paddings = { 30.f, 160.f, 20.f, 50.f }; // расстояния от границ окна
+	const float thickness = 1.f;
+	float Wx, Wy, Wcx, Wcy, frameAspect;
+	Vec2 Vc = Vec2(-2.f, -2.f), V = Vec2(4.f, 4.f);
+	Vec2 Vc_work, V_work;
+	frame_calc(paddings, Wx, Wy, Wcx, Wcy, frameAspect);
+
+	T = initT = Mat3(1.f);
+
+
+	while (!WindowShouldClose()) {
+		if(IsWindowResized()) {
+			frame_calc(paddings, Wx, Wy, Wcx, Wcy, frameAspect);
+		}
+
+		// Render figures
+		BeginDrawing();
+		ClearBackground(SKYBLUE);
+
+		DrawRectangleLinesEx({
+			Wcx, // слева
+			Wcy - Wy, // сверху
+			Wx, // ширина
+			Wy // высота
+		}, 2.f, BLACK);
+
+		Vc_work = normalize(T * Vec3(Vc, 1.f));
+		V_work = Mat2(T) * V;
+		const float center_x = Vc_work.x + V_work.x / 2;
+		const float center_y = Vc_work.y + V_work.y / 2;
+		const float delta_x = V_work.x / Wx;
+
+		Vec2 start;
+		float x, y;
+		x = Vc_work.x;
+		start.x = Wcx;
+		bool has_start = f_exists(x, delta_x), has_end, visible;
+		if(has_start) {
+			y = f(x);
+			start.y = Wcy - (y - Vc_work.y) / V_work.y * Wy;
+		}
+		
+		float delta_y;
+		unsigned char red, green, blue;
+		
+		while(start.x < Wcx + Wx) {
+			Vec2 end;
+			end.x = start.x + 1.f;
+			x += delta_x;
+			has_end = f_exists(x, delta_x);
+			if(has_end) {
+				y = f(x);
+				delta_y = (y - Vc_work.y) / V_work.y;
+				end.y = Wcy - delta_y * Wy;
+			}
+			const Vec2 tmp_end = end;
+			visible = clip(start, end, {Wcx, Wcy - Wy}, {Wcx + Wx, Wcy});
+			if(has_start && has_end && visible) {
+				if(delta_y > 1.f) delta_y = 1.f;
+				if(delta_y < 0.f) delta_y = 0.f;
+				green = 510 * delta_y;
+				if(delta_y < 0.5f) {
+					blue = 255 - green;
+					red = 0;
+				} else {
+					blue = 0;
+					red = green - 255;
+					green = 510 - green;
+				}
+				DrawLineEx(
+					{start.x, start.y},
+					{end.x, end.y},
+					thickness,
+					{red, green, blue, 255}
+				);
+			}
+			start = tmp_end;
+			has_start = has_end;
+		}
+
+		EndDrawing();
+
+		// Handle input
+		if (IsKeyPressed(KEY_C)) {
+			T = initT;
+		}
+
+		if(IsKeyDown(KEY_A)) {
+			T = translate(-V_work.x / Wx, 0.f) * T;
+		}
+
+		if(IsKeyDown(KEY_D)) {
+			T = translate(V_work.x / Wx, 0.f) * T;
+		}
+
+		if(IsKeyDown(KEY_F)) {
+			T = translate(0.f, V_work.y / Wy) * T;
+		}
+
+		if(IsKeyDown(KEY_R)) {
+			T = translate(0.f, -V_work.y / Wy) * T;
+		}
+
+		if(IsKeyDown(KEY_Z)) {
+			T = translate(-center_x, -center_y) * T;
+			T = scale(1.1f) * T;
+			T = translate(center_x, center_y) * T;
+		}
+
+		if(IsKeyDown(KEY_X)) {
+			T = translate(-center_x, -center_y) * T;
+			T = scale(1 / 1.1f) * T;
+			T = translate(center_x, center_y) * T;
+		}
+	}
+	CloseWindow();
+	NFD_Quit();
+
+	return 0;
+}
---
+++ kg/raylib/Projects/lab5_2d/matrix.hpp
@@ -0,0 +1,285 @@
+#pragma once
+
+#include <stddef.h>
+#include <raylib.h>
+
+struct Vec2 {
+    float x = 0;
+    float y = 0;
+
+    Vec2() {}
+
+    Vec2(float a, float b) : x(a), y(b) {}
+
+	Vec2& operator*=(const Vec2& v) {
+		x *= v.x;
+		y *= v.y;
+		return *this;
+	}
+
+	Vec2 operator*(const Vec2& v) const {
+		return Vec2(*this) *= v;
+	}
+
+    float &operator[](size_t i) {
+        return (reinterpret_cast<float *>(this))[i];
+    }
+};
+
+struct Vec3 {
+    float x = 0;
+    float y = 0;
+    float z = 0;
+
+    Vec3() {}
+
+    Vec3(float a, float b, float c) : x(a), y(b), z(c) {}
+
+    Vec3(Vec2 v, float c) : Vec3(v.x, v.y, c) {}
+
+    Vec3 &operator*=(const Vec3 &v) {
+        this->x *= v.x;
+        this->y *= v.y;
+        this->z *= v.z;
+        return *this;
+    }
+
+    const Vec3 operator*(const Vec3 &v) const {
+        return Vec3(*this) *= v;
+    }
+
+    float &operator[](size_t i) {
+        return (reinterpret_cast<float *>(this))[i];
+    }
+
+    const float &operator[](size_t i) const {
+        return (reinterpret_cast<const float *>(this))[i];
+    }
+};
+
+struct Vec4 {
+    float x = 0;
+    float y = 0;
+    float z = 0;
+    float w = 0;
+
+    Vec4() {}
+
+    Vec4(float a, float b, float c, float d) : x(a), y(b), z(c), w(d) {}
+
+    Vec4(Vec3 v, float d) : Vec4(v.x, v.y, v.z, d) {}
+
+    Vec4 &operator*=(const Vec4 &v) {
+        this->x *= v.x;
+        this->y *= v.y;
+        this->z *= v.z;
+        this->w *= v.w;
+        return *this;
+    }
+
+    const Vec4 operator*(const Vec4 &v) const {
+        return Vec4(*this) *= v;
+    }
+
+    float &operator[](size_t i) {
+        return (reinterpret_cast<float *>(this))[i];
+    }
+
+    const float &operator[](size_t i) const {
+        return (reinterpret_cast<const float *>(this))[i];
+    }
+};
+
+inline float dot(const Vec4 &a, const Vec4 &b) {
+    Vec4 tmp = a * b;
+    return tmp.x + tmp.y + tmp.z + tmp.w;
+}
+
+inline float dot(const Vec3 &a, const Vec3 &b) {
+    Vec3 tmp = a * b;
+    return tmp.x + tmp.y + tmp.z;
+}
+
+inline float dot(const Vec2 &a, const Vec2 &b) {
+	Vec2 tmp = a * b;
+	return tmp.x + tmp.y;
+}
+
+struct Mat4 {
+    Vec4 row1{};
+    Vec4 row2{};
+    Vec4 row3{};
+    Vec4 row4{};
+
+    Mat4() {}
+
+    Mat4(Vec4 r1, Vec4 r2, Vec4 r3, Vec4 r4) : row1(r1), row2(r2), row3(r3), row4(r4) {}
+
+    Mat4(float a) {
+        row1 = Vec4(a, 0.f, 0.f, 0.f);
+        row2 = Vec4(0.f, a, 0.f, 0.f);
+        row3 = Vec4(0.f, 0.f, a, 0.f);
+        row4 = Vec4(0.f, 0.f, 0.f, a);
+    }
+
+    Vec4 &operator[](size_t i) {
+        return (reinterpret_cast<Vec4 *>(this))[i];
+    }
+
+    const Vec4 &operator[](size_t i) const {
+        return (reinterpret_cast<const Vec4 *>(this))[i];
+    }
+
+    Mat4 &transpose() {
+        Mat4 tmp(*this);
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                (*this)[i][j] = tmp[j][i];
+            }
+        }
+        return *this;
+    }
+
+    const Vec4 operator*(const Vec4 &v) const {
+        Vec4 res{};
+        for (int i = 0; i < 4; ++i) {
+            res[i] = dot((*this)[i], v);
+        }
+        return res;
+    }
+
+    Mat4 &operator*=(const Mat4 &m) {
+        Mat4 A(*this);
+        Mat4 B(m);
+        B.transpose();
+        for (int i = 0; i < 4; i++) {
+            (*this)[i] = A * B[i];
+        }
+        return this->transpose();
+    }
+
+    const Mat4 operator*(const Mat4 &m) const {
+        return Mat4(*this) *= m;
+    }
+};
+
+struct Mat3 {
+    Vec3 row1{};
+    Vec3 row2{};
+    Vec3 row3{};
+
+    Mat3() {}
+
+    Mat3(Vec3 r1, Vec3 r2, Vec3 r3) : row1(r1), row2(r2), row3(r3) {}
+
+	Mat3(Mat4 m) : 
+		row1({m.row1.x, m.row1.y, m.row1.z}),
+		row2({m.row2.x, m.row2.y, m.row2.z}),
+		row3({m.row3.x, m.row3.y, m.row3.z}) {};
+
+    Mat3(float a) {
+        row1 = Vec3(a, 0.f, 0.f);
+        row2 = Vec3(0.f, a, 0.f);
+        row3 = Vec3(0.f, 0.f, a);
+    }
+
+    Vec3 &operator[](size_t i) {
+        return (reinterpret_cast<Vec3 *>(this))[i];
+    }
+
+    const Vec3 &operator[](size_t i) const {
+        return (reinterpret_cast<const Vec3 *>(this))[i];
+    }
+
+    Mat3 &transpose() {
+        Mat3 tmp(*this);
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                (*this)[i][j] = tmp[j][i];
+            }
+        }
+        return *this;
+    }
+
+    const Vec3 operator*(const Vec3 &v) const {
+        Vec3 res{};
+        for (int i = 0; i < 3; ++i) {
+            res[i] = dot((*this)[i], v);
+        }
+        return res;
+    }
+
+    Mat3 &operator*=(const Mat3 &m) {
+        Mat3 A(*this);
+        Mat3 B(m);
+        B.transpose();
+        for (int i = 0; i < 3; i++) {
+            (*this)[i] = A * B[i];
+        }
+        return this->transpose();
+    }
+
+    const Mat3 operator*(const Mat3 &m) const {
+        return Mat3(*this) *= m;
+    }
+};
+
+struct Mat2 {
+    Vec2 row1{};
+    Vec2 row2{};
+
+    Mat2() {}
+
+    Mat2(Vec2 r1, Vec2 r2) : row1(r1), row2(r2) {}
+
+    Mat2(float a) {
+        row1 = Vec2(a, 0.f);
+        row2 = Vec2(0.f, a);
+    }
+
+	Mat2(Mat3 m) : row1({m.row1.x, m.row1.y}), row2({m.row2.x, m.row2.y}) {}
+
+    Vec2 &operator[](size_t i) {
+        return (reinterpret_cast<Vec2 *>(this))[i];
+    }
+
+    const Vec2 &operator[](size_t i) const {
+        return (reinterpret_cast<const Vec2 *>(this))[i];
+    }
+
+    Mat2 &transpose() {
+        Mat2 tmp(*this);
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                (*this)[i][j] = tmp[j][i];
+            }
+        }
+        return *this;
+    }
+
+    const Vec2 operator*(const Vec2 &v) const {
+        Vec2 res{};
+        for (int i = 0; i < 2; ++i) {
+            res[i] = dot((*this)[i], v);
+        }
+        return res;
+    }
+
+    Mat2 &operator*=(const Mat2 &m) {
+        Mat2 A(*this);
+        Mat2 B(m);
+        B.transpose();
+        for (int i = 0; i < 2; i++) {
+            (*this)[i] = A * B[i];
+        }
+        return this->transpose();
+    }
+
+    const Mat2 operator*(const Mat2 &m) const {
+        return Mat2(*this) *= m;
+    }
+};
+
+inline Vec2 normalize(const Vec3 &v) {
+    return Vec2(v.x / v.z, v.y / v.z);
+}
