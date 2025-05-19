c2b008392368cc36eec205602d9dc23e3965e6c5
Данила Григорьев <danila.sar@yandex.ru>
2025-05-19 11:07:12 +0400

    фича(кг): шейдеры

--- kg/opengl/Projects/lab7/main.cpp
+++ kg/opengl/Projects/lab7/main.cpp
@@ -4,6 +4,29 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+//=====================================================================
+// ВЕРШИННЫЙ ШЕЙДЕР
+//=====================================================================
+const GLchar *vertexShaderSource
+    = "#version 330 core\n"
+      "layout (location = 0) in vec3 position;\n"
+      "void main() {\n"
+      "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+      "}\0";
+//=====================================================================
+//======================================================
+// ФРАГМЕНТНЫЙ ШЕЙДЕР
+//======================================================
+const char *fragmentShaderSource
+    = "#version 330 core\n"
+      "out vec4 color;\n"
+      "void main() {\n"
+      "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+      "}\n\0";
+
+//=======================================================
+const GLchar **abaunda = &vertexShaderSource;
+
 // обработчик события Resize
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -23,6 +46,34 @@ void key_callback(
         default:
             break;
         }
+    }
+}
+
+enum ShaderType {
+    VERTEX,
+    FRAGMENT
+};
+
+GLuint compile_shader(const GLchar* source, ShaderType type) {
+    GLuint shader; // шейдерный объект - вершинный шейдер
+    shader = glCreateShader(GL_VERTEX_SHADER); // создаем объект
+    // привязываем исходный код к шейдерному объекту
+    glShaderSource(shader, 1, &source, NULL);
+    glCompileShader(shader);
+    GLint success; // результат компиляции
+    // запрашивам статус компиляции шейдера в переменную success
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(!success) {
+        GLchar info_log[512];
+        glGetShaderInfoLog(shader, 512, nullptr, info_log); // запрашиваем сообщение
+        // выводим сообщение об ошибке на экран
+        std::string str_name;
+        switch(type) {
+            case VERTEX: str_name = "VERTEX"; break;
+            case FRAGMENT: str_name = "FRAGMENT"; break;
+            default: str_name = "UNDEFINED_SHADER"; break;
+        }
+        std::cerr << "ERROR::SHADER::" << str_name << "::COMPILATION_FAILED\n" << info_log << std::endl;
     }
 }
 
@@ -55,6 +106,7 @@ int main() {
     // Назначение обработчика нажатия клавиш
     glfwSetKeyCallback(window, key_callback);
 
+
     if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
         std::cerr << "ERROR: can't start GLAD" << std::endl;
         glfwTerminate();
@@ -63,6 +115,7 @@ int main() {
 
     glViewport(0, 0, window_width, window_height);
 
+//
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0, 0, 0, 1);
         glClear(GL_COLOR_BUFFER_BIT);
